75146a2a80dcf5269f04f5632deb7c6433f9e010
T D Nuwan Chathuranga <nchathu2014@gmail.com>
2020-11-23 08:04:53 +0530

    build to deploy

---
+++ build/precache-manifest.57d985c7ea3640febe7d6b2e49ad8375.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e41d7c345ad5d7c59809",
+    "url": "/static/js/main.e41d7c34.chunk.js"
+  },
+  {
+    "revision": "c5d7b67e86c284b1b3cc",
+    "url": "/static/js/1.c5d7b67e.chunk.js"
+  },
+  {
+    "revision": "e41d7c345ad5d7c59809",
+    "url": "/static/css/main.6b8ae1b3.chunk.css"
+  },
+  {
+    "revision": "e7aa92ab94dcc539b079465e0dbac355",
+    "url": "/index.html"
+  }
+];
